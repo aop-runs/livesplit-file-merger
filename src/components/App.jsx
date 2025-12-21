59165da516d49676d45ed2831bafeda1747b1c64
AnOrdinaryPerson <73274191+aop-runs@users.noreply.github.com>
2025-12-21 17:48:50 -0500

    Rename project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { ListContainer } from './Splits/ListContainer'
+import '../styles/style.scss'
 
 export const App = () => {
   return (
