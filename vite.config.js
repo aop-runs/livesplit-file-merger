e55df70822c81713f95ce93a8d4205552b5e6098
AnOrdinaryPerson <73274191+aop-runs@users.noreply.github.com>
2025-12-31 00:25:56 -0500

    Fix base in config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/livesplit-split-merge-site/",
+  base: "/livesplit-file-merger/",
   plugins: [react()],
 })
